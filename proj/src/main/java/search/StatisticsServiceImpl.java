8ea3fdb919b00636ad65eaa84dbf1eb67f89f488
J0aoDias <joaodias0012@gmail.com>
2025-03-25 07:18:18 +0000

    stats atualizadas em tempor real corrigidas

--- proj/src/main/java/search/StatisticsServiceImpl.java
+++ proj/src/main/java/search/StatisticsServiceImpl.java
@@ -1,4 +1,5 @@
 package search;
+
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
@@ -9,63 +10,59 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-
-public class StatisticsServiceImpl extends UnicastRemoteObject implements StatisticsService{
+public class StatisticsServiceImpl extends UnicastRemoteObject implements StatisticsService {
     private List<StatisticsClient> subscribers;
     private Statistics currentStats;
-    public StatisticsServiceImpl() throws RemoteException{
+
+    public StatisticsServiceImpl() throws RemoteException {
         super();
         subscribers = new ArrayList<>();
     }
+
     @Override
-    public void subscribeStatistics(StatisticsClient client) throws RemoteException{
+    public void subscribeStatistics(StatisticsClient client) throws RemoteException {
         subscribers.add(client);
         System.out.println("Cliente inscrito para receber estatisticas");
     }
 
-    public void notifyStats(Statistics stats){
+    public void notifyStats(Statistics stats) {
         currentStats = stats;
-        Iterator<StatisticsClient> iterator = subscribers.iterator();
-        while (iterator.hasNext()) {
+        Iterator<StatisticsClient> it = subscribers.iterator();
+        while (it.hasNext()) {
             try {
-                StatisticsClient client = iterator.next();
-                client.updateStats(stats);
+                it.next().updateStats(stats);
             } catch (RemoteException e) {
-                iterator.remove();
+                it.remove();
                 System.out.println("Removido cliente inacessível.");
             }
         }
     }
+
     @Override
-    public Statistics getStats() throws RemoteException {
-        if (currentStats != null) {
-            return currentStats;
-        } else {
-            try {
-                Registry registry = LocateRegistry.getRegistry(1099);
-                String[] allNames = registry.list();
-                Map<String, Integer> barrelIndexSizes = new HashMap<>();
-                for (String name : allNames) {
-                    if (name.startsWith("Barrel")) {
-                        try {
-                            IBarrelGateway barrel = (IBarrelGateway) registry.lookup(name);
-                            int size = barrel.getIndexSize();
-                            barrelIndexSizes.put(name, size);
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
+    public synchronized Statistics getStats() throws RemoteException {
+        try {
+            Registry registry = LocateRegistry.getRegistry(1099);
+            String[] names = registry.list();
+            Map<String, Integer> barrelIndexSizes = new HashMap<>();
+            for (String name : names) {
+                if (name.startsWith("Barrel")) {
+                    try {
+                        IBarrelGateway barrel = (IBarrelGateway) registry.lookup(name);
+                        barrelIndexSizes.put(name, barrel.getIndexSize());
+                    } catch (Exception e) {
+                        System.err.println("Erro ao obter índice de " + name + ": " + e.getMessage());
                     }
                 }
-                
-                Statistics stats = new Statistics(0, barrelIndexSizes.size(), 0);
-                stats.setTop10Searches(new ArrayList<>());
-                stats.setBarrelIndexSizes(barrelIndexSizes);
-                return stats;
-            } catch (Exception e) {
-                throw new RemoteException("Erro ao obter as estatísticas", e);
             }
+            if (currentStats == null) {
+                currentStats = new Statistics(0, barrelIndexSizes.size(), 0);
+                currentStats.setTop10Searches(new ArrayList<>());
+            }
+            currentStats.setActiveBarrels(barrelIndexSizes.size());
+            currentStats.setBarrelIndexSizes(barrelIndexSizes);
+            return currentStats;
+        } catch (Exception e) {
+            throw new RemoteException("Erro ao obter as estatísticas", e);
         }
     }
-
 }
-
