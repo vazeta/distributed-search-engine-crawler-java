a04f9ec1765310b350d009ee1b9c5aa6026e40ef
João Vaz <joao.vaz1810@gmail.com>
2025-04-04 20:31:28 +0100

    mais updates -> nao tenho ja erros no downloader e assim

--- proj/src/main/java/com/example/googol/controllers/HomeController.java
+++ proj/src/main/java/com/example/googol/controllers/HomeController.java
@@ -1,12 +1,6 @@
 package com.example.googol.controllers;
-import java.rmi.RemoteException;
 import org.springframework.stereotype.Controller;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.ui.Model;
-import java.util.List;
-import search.*;
 
 
 @Controller
